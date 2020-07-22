efafca293b2fbf8a235020f149d7c64c48137e17
Suvendu97 <57838911+Suvendu97@users.noreply.github.com>
2020-07-23 02:22:03 +0530

    view engine setup and files added (ejs)
    ejs files added in views folder (view engine folder)

--- controllers/address_controller.js
+++ controllers/address_controller.js
@@ -1,7 +1,10 @@
 
 
 module.exports.address = function(req, res) {
-    res.end('<h1>I am from Kolkata</h1>');
+    //res.end('<h1>I am from Kolkata</h1>');
+    return res.render('address', {
+        title : "Users location"
+    });
 }
 
 //now http://localhost:8000/address/location the search completed and it will send the response to the browser
--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 8000;
 //any request comes in, it will send that to routes index.js
 app.use('/',require('./routes'));
 
+
+//setup the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err) {
     if(err) {
         console.log(`Error in running the server: ${err}`);
