a84e81dc9ab965a19a6c31128c08608d34e83529
Suvendu97 <57838911+Suvendu97@users.noreply.github.com>
2020-07-25 00:22:05 +0530

    local-authentication

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,10 @@ router.get('/sign-in', usersController.signIn);
 
 router.post('/create',usersController.create);
 
+//setting up the path for create-session(in user_controller sign in section)
+router.post('/create-session', usersController.createSession);
+
+//sign out
+router.get('/sign-out', usersController.destroySession);
+
 module.exports = router;
