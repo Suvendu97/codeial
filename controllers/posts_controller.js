1ddfb3056da7f4ce1620693ff262e10a1fa4d22b
Suvendu97 <57838911+Suvendu97@users.noreply.github.com>
2020-07-29 17:23:33 +0530

    delete post function

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
 // const user = require('../models/user');
+const Comment = require('../models/comment');
 
 // to create a post in DB
 module.exports.create = function(req, res) {
@@ -13,4 +14,22 @@ module.exports.create = function(req, res) {
 
         return res.redirect('back');
     }); 
+}
+
+
+module.exports.destroy = function(req, res) {
+    Post.findById(req.params.id, function(err, post) {
+        //  ideally we should be doing ._id
+        //  but when I'm comparing two ids of two objects we need to convert them into string
+        // .id means converting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        } else {
+            return res.redirect('back');
+        }
+    });
 }
