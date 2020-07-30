b1eb3acb1c7640e2ee0bf63c0d336038de003b85
Suvendu97 <57838911+Suvendu97@users.noreply.github.com>
2020-07-30 16:04:37 +0530

    Converting to Async Await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,7 @@ const Post = require('../models/post');
 const User = require('../models/user');
 
 //exports home function which renders a view call home using ejs
-module.exports.home = function(req, res) {
+module.exports.home = async function(req, res) {
     // console.log(req.cookies);
     // res.cookie('user_id',25);
     //return res.end('<h1>Express is up for codeial!</h1>');
@@ -15,24 +15,32 @@ module.exports.home = function(req, res) {
     // });
 
     // finding all the post and populating user of each post, after that I'm doing call back
-    Post.find({})
-    .populate('user')
-    .populate({        //preloading two models, 1.comments and user of the comments
-        path: 'comments',
-        populate: {
-            path:'user'
-        }
-    })
-    .exec(function(err, posts){
-
-        User.find({}, function(err, users) {
-            return res.render('home', {
-                title : "Codeial | Home",   // these are response locals we can only access in font page template 
-                posts: posts,
-                all_users: users
-            });
+    try {
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({        //preloading two models, 1.comments and user of the comments
+            path: 'comments',
+            populate: {
+                path:'user'
+            }
         });
-    
-    })
+        // .exec(function(err, posts){
+
+            
+        
+        // })
+        let users = await User.find({});
+        
+        return res.render('home', {
+            title : "Codeial | Home",   // these are response locals we can only access in font page template 
+            posts: posts,
+            all_users: users
+        });
+
+    } catch(err) {
+        console.log('Error', err);
+        return;
+    }
+
     
 }
