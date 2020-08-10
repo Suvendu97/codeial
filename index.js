9dcec7d5e0f23502a8c4bd941e5c8922288791df
Suvendu97 <57838911+Suvendu97@users.noreply.github.com>
2020-08-11 01:54:28 +0530

    setting up passport-JWT

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 // store the session info even when the server restarts it remains in db so that  signed in user don't get signed out in case the server restarts
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
