58d09c8b1913800766eb2539d0be2d238233f230
Suvendu97 <57838911+Suvendu97@users.noreply.github.com>
2020-09-19 16:34:42 +0530

    likes controller and routes added

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.use('/address',require('./address'));
 
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
+router.use('/likes', require('./likes'));
 
 router.use('/api', require('./api'));
 
