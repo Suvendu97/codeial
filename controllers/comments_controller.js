c9b174b17bff8933982ee27274c6276413827907
Suvendu97 <57838911+Suvendu97@users.noreply.github.com>
2020-09-19 17:10:54 +0530

    likes count and final solution

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -3,6 +3,7 @@ const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
 const queue = require('../config/kue');
 const commentEmailWorker = require('../workers/comment_email_worker');
+const Like = require('../models/like');
 
 
 module.exports.create = async function(req, res){
@@ -62,6 +63,9 @@ module.exports.destroy = async function(req, res){
             comment.remove();
     
             let post = await Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // CHANGE :: destroy the associated likes for this comment
+            await Like.deleteMany({likeable: comment._id, onModel: 'Comment'});
             
 
             //send the comment id which was deleted in response to ajax call
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,15 +4,6 @@ const User = require('../models/user');
 
 //exports home function which renders a view call home using ejs
 module.exports.home = async function(req, res) {
-    // console.log(req.cookies);
-    // res.cookie('user_id',25);
-    //return res.end('<h1>Express is up for codeial!</h1>');
-    // Post.find({}, function(err, posts) {
-    //     return res.render('home', {
-    //         title : "Codeial | Home",
-    //         posts: posts
-    //     });
-    // });
 
     // finding all the post and populating user of each post, after that I'm doing call back
     try{
@@ -24,8 +15,12 @@ module.exports.home = async function(req, res) {
            path: 'comments',
            populate: {
                path: 'user'
+           },
+           populate: {
+               path: 'likes'
            }
-       });
+       }).populate('comments')
+       .populate('likes');
    
        let users = await User.find({});
 
